21475be55f045553b0f4f8ae3815a13403f976b7
Nathan Moura <nathanmouramotta@gmail.com>
2026-04-25 11:20:28 -0300

    Create Q4.py

---
+++ Aula 01 - Atividade/Q4.py
@@ -0,0 +1,6 @@
+salarioHora = int(input("Digite qual seu salário por hora"))
+horasTrabalho = int(input("Digite quantas horas você trabalha por mês"))
+
+salario = salarioHora * horasTrabalho
+
+print(f"Seu salário é {salario}")
