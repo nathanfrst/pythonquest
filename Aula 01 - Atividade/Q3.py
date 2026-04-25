e65348899a24dea1773af154ac104a1e61bc41eb
Nathan Moura <nathanmouramotta@gmail.com>
2026-04-25 11:19:55 -0300

    Create Q3.py

---
+++ Aula 01 - Atividade/Q3.py
@@ -0,0 +1,5 @@
+ladoQuad = float(input("Digite o lado do quadrado para saber sua área: "))
+areaQuad = ladoQuad * ladoQuad
+dobroArea = areaQuad * 2
+
+print(f"A área do quadrado é {areaQuad} e o dobro de sua área é {dobroArea}")
